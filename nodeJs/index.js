9a0cbf4f6099382832905e5aabf35d2f72d47324
Franswa <jonathan.cardona0612@gmail.com>
2022-07-11 16:19:27 -0500

    new update nodeJs

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -2,6 +2,22 @@ const http = require("http");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 
+
+let resources = {
+    events: [{name:'Marina',
+    service:'Furniture',
+    specific:'Chair',
+    radio:'Bello'},
+    {name:'Stella',
+    service:'food',
+    specific:'Meal and wine',
+    radio:'Envigado'}]
+
+}
+
+
+
+
 const requestP = (req, res) =>{
     const urlCurrent = req.url;
     const urlParse = url.parse(urlCurrent, true);
@@ -17,19 +33,32 @@ const requestP = (req, res) =>{
     });
     req.on("end",()=>{
         buffer += decoder.end();
-    const data = {
-        rout: cleanRout,
+        if (headers['content-type'] === 'application/json'){
+            buffer = JSON.parse(buffer)
+        }
+        if(cleanRout.indexOf("/") > -1){
+            var [firstRout, indice] = cleanRout.split('/');
+        }
+        const data = {
+        indice,
+        rout: firstRout || cleanRout, 
         query,
         method,
-        headers,
-         payload: buffer
+        payload: buffer
     };
+
+ 
+
+    console.log({data})
+   
+
     let handler;
-    if(cleanRout && router[cleanRout]){
-        handler = router[cleanRout]
+    if(data.rout && router[data.rout] && router[data.rout][method]){
+        handler = router[data.rout][method]
     }else{
         handler = router.noFound
     }
+    
     if(typeof handler === 'function'){
         handler(data, (statusCode= 200, message)=>{
             const response = JSON.stringify(message);
@@ -43,10 +72,23 @@ const requestP = (req, res) =>{
 const router = {
     route: (data, callback)=>
     callback(200,{message: 'this is /rout'}),
-    users: (data, callback)=>
-    callback(200,[{name: 'user 1'},{name: 'user 3'},{name: 'user 2'}]),
-    noFound: (data, callback)=>
-    callback(404,{message: 'no found'})
+    users: (data, callback)=>{
+    callback(200,[{name: 'user 1'},{name: 'user 3'},{name: 'user 2'}])},
+    events: {
+        GET:(data, callback)=>
+        {if(typeof data.indice !== "undefined"){
+            if(resources.events[data.indice])
+            {return callback(200,resources.events[data.indice])}
+            return callback(404, {message:`Event with index ${data.indice} no found`})
+        }
+        callback(200,resources.events);
+        },
+        POST:(data, callback)=>
+        {   resources.events.push(data.payload);
+            callback(201, data.payload)}
+        },
+    noFound: (data, callback)=>{
+    callback(404,{message: 'no found'})}
 }
 
 const server = http.createServer(requestP)
