e1c08b8cd8b3eeed3564ecaec913457ec976c029
Franswa <jonathan.cardona0612@gmail.com>
2022-07-12 00:01:02 -0500

    Edit events

--- nodeJs/router.js
+++ nodeJs/router.js
@@ -3,7 +3,7 @@ module.exports = {
     callback(200,{message: 'this is /rout'}),
     users: (data, callback)=>{
     callback(200,[{name: 'user 1'},{name: 'user 3'},{name: 'user 2'}])},
-    events: {
+    events:{
         GET:(data, callback)=>
         {if(typeof data.indice !== "undefined"){
             if(global.resources.events[data.indice])
@@ -15,7 +15,16 @@ module.exports = {
         POST:(data, callback)=>
         {   global.resources.events.push(data.payload);
             callback(201, data.payload)}
+        ,
+        PUT:(data, callback)=>
+        {if(typeof data.indice !== "undefined"){
+            if(global.resources.events[data.indice])
+            {   global.resources.events[data.indice] = data.payload;
+                return callback(200,global.resources.events[data.indice])}
+            return callback(404, {message:`Event with index ${data.indice} no found`})
+        }
+        callback(400,{message: "index no sent"});
         },
-    noFound: (data, callback)=>{
-    callback(404,{message: 'no found'})}
+        noFound: (data, callback)=>{
+        callback(404,{message: 'no found'})}}
 }
